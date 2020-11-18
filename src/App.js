acd639046ffdb66c1ffa4a16029c2be6610f596c
nostam <matsonho@gmail.com>
2020-11-18 15:27:34 +0100

    a start for ex2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import WarningSign from "./components/WarningSign";
 import { Container } from "react-bootstrap";
+import MyBadge from "./components/MyBadge";
+import WarningSign from "./components/WarningSign";
+
 class App extends React.Component {
   state = {
     variant: "danger",
@@ -13,6 +15,7 @@ class App extends React.Component {
       <>
         <Container>
           <WarningSign text={this.state.text} variant={this.state.variant} />
+          <MyBadge />
         </Container>
       </>
     );
---
+++ src/components/MyBadge.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Badge } from "react-bootstrap";
+
+const myBadge = () => {
+  return <Badge></Badge>;
+};
+export default myBadge;
