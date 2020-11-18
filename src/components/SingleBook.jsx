437479224ae9b72df678348fce6bf7cf7dc5741b
nostam <matsonho@gmail.com>
2020-11-18 19:10:12 +0100

    add const in SingleBook

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -2,27 +2,25 @@ import React from "react";
 import { Card, Button, Col } from "react-bootstrap";
 
 function SingleBook(props) {
-    console.log(props);
-
-    return (
-        <>
-            <Col key={props.book.asin.toString()}>
-                <Card style={{ width: "15rem" }}>
-                    <Card.Img variant="top" src={props.book.img} />
-                    <Card.Body>
-                        <Card.Title>{props.book.title}</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-          </Card.Text>
-                        <Button variant="primary">Buy @ ${props.book.price}</Button>
-                    </Card.Body>
-                    <Card.Footer className="text-right">
-                        ASIN: {props.book.asin}
-                    </Card.Footer>
-                </Card>
-            </Col>
-        </>
-    );
+  console.log(props);
+  const book = props.book;
+  return (
+    <>
+      <Col key={book.asin.toString()}>
+        <Card style={{ width: "15rem" }}>
+          <Card.Img variant="top" src={book.img} />
+          <Card.Body>
+            <Card.Title>{book.title}</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Buy @ ${book.price}</Button>
+          </Card.Body>
+          <Card.Footer className="text-right">ASIN: {book.asin}</Card.Footer>
+        </Card>
+      </Col>
+    </>
+  );
 }
 export default SingleBook;
